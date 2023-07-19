cd81bbcc0439c5e10ee9b341df6f55fde4318954
seungbopark <33363955+seungbpark@users.noreply.github.com>
2023-07-19 10:27:52 +0900

    Add files via upload
    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+// go mod 생성
+//go mod init example.com/m
+//go mod tidy
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Servic Started")
+	//s := new(router.Service)
+	//s.SetRouter()
+	//s.Router.Run(":8081")
+}
